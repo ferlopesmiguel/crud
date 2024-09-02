9523297f9f141899e11c094f9c61a6ad011684e2
Fernando Miguel <fer_lop10@hotmail.com>
2024-09-02 11:54:06 -0300

    update

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+#business rules
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class ProductModel(Base):
+    __tablename__ = 'products' #table name
+
+    id = Column(Integer)
+    name = Column(String)
+    description = Column(String)
+    price = Column(Float)
+    category = Column(String)
+    email_supplier = Column(String)
+    created_at = Column(DateTime(timezone=True), default=func.now())
